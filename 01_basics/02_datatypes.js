3dd00a117eb63812ae97414df76b0cd01545fa19
Anant panwar <anantpanwar150@gmail.com>
2026-04-24 15:23:09 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"// trate all JS code as new version
+
+//alert( 3+ 3) // we are using node.js not browser
+
+console.log("Anant")
+
+console.log(3+3)//code readablity should be high
+
+let name ="Anant"
+let age =21
+let isLoggedIn= false
+
+// number => 2 to the power of 53
+// bigint
+//string => ""
+// boolean => return true/false
+// null => Standalone value
+// undefiend =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined);//undefiend
+console.log(typeof object)//undefiend
+console.log(typeof null)//object
